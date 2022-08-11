e01c1de25a6155282fe4c0ddf1aa4648cf2eb865
Tattisha <4artfloris@gmail.com>
2022-08-11 23:15:08 +0300

    added units conversion

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ function clickSearch(event) {
   city = `${city.value}`;
   searchCity(city);
 }
+
 let searching = document.querySelector("#form-search-city");
 searching.addEventListener("submit", clickSearch);
 
@@ -83,3 +84,33 @@ function showGeo(position) {
 function getGeo() {
   navigator.geolocation.getCurrentPosition(showGeo);
 }
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temp-left");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temp-left");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperature.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
+
+let button = document.querySelector("#current-button");
+button.addEventListener("click", getGeo);
+
+searchCity("London");
