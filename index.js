896c608846879e17aa0f06d72f6ab91e454a1ca0
Tattisha <4artfloris@gmail.com>
2022-08-11 22:44:41 +0300

    created index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let now = new Date();
+let h2 = document.querySelector("h2");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+h2.innerHTML = `${day} ${hours}:${minutes}`;
