a18420f388d16f9a0139ce33b012df00d5450120
Tattisha <4artfloris@gmail.com>
2022-08-11 22:51:53 +0300

    added function showWeather

--- index.js
+++ index.js
@@ -19,3 +19,36 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 h2.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showWeather(response) {
+  let city = document.querySelector("#current-city");
+  city.innerHTML = response.data.name;
+
+  celsiusTemp = response.data.main.temp;
+
+  let temperature = document.querySelector("#temp-left");
+  temperature.innerHTML = Math.round(celsiusTemp);
+
+  let maxTemp = document.querySelector("#high-temp");
+  maxTemp.innerHTML = Math.round(response.data.main.temp_max);
+
+  let minTemp = document.querySelector("#low-temp");
+  minTemp.innerHTML = Math.round(response.data.main.temp_min);
+
+  let rain = document.querySelector("#rain");
+  rain.innerHTML = response.data.clouds.all;
+
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+
+  getForecast(response.data.coord);
+}
